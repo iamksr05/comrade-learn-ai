1703bac1de201c92e37391b3599274f81a10b2f0
Karan Ram <Karanram730005@gmail.com>
2025-11-10 15:22:48 +0530

    first commit

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,13 @@
+/**
+ * PostCSS configuration for processing CSS
+ * Used by Vite to transform Tailwind CSS and apply autoprefixer
+ *
+ * @type {import('postcss-load-config').Config}
+ */
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 };
+
